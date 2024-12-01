bdb6ace998d710195544faafbc570f5b2de5f0ff
chinmayee yanamadala <93443300+whatnin@users.noreply.github.com>
2024-12-01 18:48:35 -0500

    Delete survey directory

--- survey/src/main/java/assignment3/survey/repository/SurveyRepository.java
+++
@@ -1,9 +0,0 @@
-package assignment3.survey.repository;
-
-import assignment3.survey.entity.Survey;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SurveyRepository extends JpaRepository<Survey, Long> {
-}
